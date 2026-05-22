0caecd8d9b318afd5de4d3b68e0f8c2581b8bd1f
Brandon Villalobos <villalobosbe498@gmail.com>
2026-05-21 21:54:32 -0600

    Add files via upload

--- Tarea6/Tarea6.cpp
+++ Tarea6/Tarea6.cpp
@@ -5,7 +5,9 @@
 #include <ctime> 
 #include "ArrayList.h"
 #include "LinkedList.h"
+#include <chrono>
 
+using namespace std::chrono;
 using std::cin;
 using std::cout;
 using std::endl;
@@ -14,37 +16,59 @@ using std::runtime_error;
 using std::getline;
 using std::ws;
 
-void radixSort(ArrayList<int>* lista, int base) {
-    ArrayList<LinkedList<int>*>* buckets;
-    int digitos = getCantDigitos(lista, base);
-    for (int i = 0; i < base i++) {
-        buckets->append(new LinkedList<int>()); 
-    }
-    for (int i = 0; i < digitos; i++) {
-        for (lista->goToStart(); !lista->atEnd(); lista->next()) {
-            int numero = lista->getElement();
-            int digito = numero % base;
-            buckets[digito].append(numero);
-        }
-    }
-    for (int i = 0; i < base; i++) delete buckets->getAt(i);
-    delete buckets;
-}
-
-int getCantDigitos(ArrayList<int>* lista, int base) {
-    int res = 0;
+int getCantDigitos(List<int>* lista, int base) {
+    int mayor = -1;
     for (lista->goToStart(); !lista->atEnd(); lista->next()) {
         int num = lista->getElement();
-        if (num > res)
-            res = num;
+        if (num > mayor)
+            mayor = num;
     }
-    int digits = 1;
-    while (res) {
-        res /= base;
+    int digits = 0;
+    while (mayor) {
+        mayor /= base;
         digits++;
     }
     return digits;
 }
+
+
+void radixSort(List<int>* lista, int base, int sizeLista) {
+    ArrayList<LinkedList<int>*>* buckets = new ArrayList<LinkedList<int>*>(base);
+    int digitos = getCantDigitos(lista, base);
+    for (int i = 0; i < base; i++) {
+        buckets->append(new LinkedList<int>()); 
+    }
+    int divisor = 1;
+    for (int i = 0; i < digitos; i++) {
+        for (lista->goToStart(); !lista->atEnd(); lista->next()) {
+            int numero = lista->getElement();
+            int digito = (numero / divisor) % base;
+            buckets->goToPos(digito);
+            List<int>* bucket = buckets->getElement();
+            bucket->append(numero);
+        }
+		lista->clear();
+        buckets->goToStart();
+        for (buckets->goToStart(); !buckets->atEnd(); buckets->next()) {
+            LinkedList<int>* bucket = buckets->getElement();
+            bucket->goToStart();
+            int sizeBucket = bucket->getSize();
+            for (int i = 0; i < sizeBucket; i++) {
+                int numero = bucket->remove();
+                lista->append(numero);
+            }
+        }
+        divisor *= base;
+    }
+    buckets->goToStart();
+    for (int i = 0; i < base; i++) {
+        LinkedList<int>* bucket = buckets->getElement();
+        delete bucket;
+        buckets->next();
+    }
+    delete buckets;
+}
+
 
 int main() {
     ArrayList<int>* Lista;
@@ -54,25 +78,39 @@ int main() {
     string repetir = "s";
     try {
         while (repetir == "s" || repetir == "S") {
-            cout << "Tamano de la lista: ";
-            getline(cin, temp);
+            cout << "Tamano de la nueva lista: ";
+            getline(cin >> ws, temp);
             sizeLista = stoi(temp);
+            if (sizeLista < 1)
+                throw runtime_error("Tamano debe ser mayor o igual 1.");
             cout << "Base numerica: ";
-            getline(cin, temp);
+            getline(cin >> ws, temp);
             base = stoi(temp);
+            if (base < 2)
+                throw runtime_error("La base debe ser mayor o igual a 2.");
             Lista = new ArrayList<int>(sizeLista);
             srand(time(0)); 
             for (int i = 0; i < sizeLista ; i++) {
-                int numeroRandom = rand() % 10000;
+                int numeroRandom = rand() % 100000;
                 Lista->append(numeroRandom);
             }
             cout << "Lista Random desordenada: ";
             Lista->print();
-            radixSort(Lista, base);
+            auto inicio = high_resolution_clock::now();
+            radixSort(Lista, base, sizeLista);
+            auto fin = high_resolution_clock::now();
+            auto duracion = duration_cast<milliseconds>(fin - inicio);
+            cout << "\nLista ordenada: \\n";
+            Lista->print();
+            cout << "Tiempo: " << duracion.count() << " ms\n";
+            delete Lista;
+            cout << "\nDesea repetir? (s/n) ";
+            getline(cin >> ws, repetir);
         }
     }
     catch (const runtime_error& e) {
         cout << "ERROR: " << e.what() << endl;
     }
-
+    cout << "Gracias por usar el programa :)" << endl;
+    return 0;
 }
